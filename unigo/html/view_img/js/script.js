559f08b4f0d85d05bcd37ef168d6edc67a3d55d2
sieunhantrieu <thanhtrieu118@yahoo.com>
2022-07-18 15:52:11 +0700

    update final 2

---
+++ unigo/html/view_img/js/script.js
@@ -0,0 +1,196 @@
+$(document).ready(function(){
+    $('.slide-show').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
+
+    $('.slide-menu').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            
+        ]
+    });
+
+    $('.brand-slide').slick({
+        slidesToShow: 8,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
+    });
+
+    $('.news-ts').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+    
+});
+
+(function ($) {
+    $.fn.aceResponsiveMenu = function (options) {
+
+        //plugin's default options
+        var defaults = {
+            resizeWidth: '991',
+            animationSpeed: 'fast',
+            accoridonExpAll: false
+        };
+
+        //Variables
+        var options = $.extend(defaults, options),
+            opt = options,
+            $resizeWidth = opt.resizeWidth,
+            $animationSpeed = opt.animationSpeed,
+            $expandAll = opt.accoridonExpAll,
+            $aceMenu = $(this),
+            $menuStyle = $(this).attr('data-menu-style');
+
+        // Initilizing        
+        $aceMenu.find('ul').addClass("sub-menu");
+        $aceMenu.find('ul').siblings('a').append('<span class="arrow "></span>');
+        if ($menuStyle == 'accordion') { $(this).addClass('collapse'); }
+
+        // Window resize on menu breakpoint 
+        if ($(window).innerWidth() <= $resizeWidth) {
+            menuCollapse();
+        }
+        $(window).resize(function () {
+            menuCollapse();
+        });
+
+        // Menu Toggle
+        function menuCollapse() {
+            var w = $(window).innerWidth();
+            if (w <= $resizeWidth) {
+                $aceMenu.find('li.menu-active').removeClass('menu-active');
+                $aceMenu.find('ul.slide').removeClass('slide').removeAttr('style');
+                $aceMenu.addClass('collapse hide-menu');
+                $aceMenu.attr('data-menu-style', '');
+                $('.menu-toggle').show();
+            } else {
+                $aceMenu.attr('data-menu-style', $menuStyle);
+                $aceMenu.removeClass('collapse hide-menu').removeAttr('style');
+                $('.menu-toggle').hide();
+                if ($aceMenu.attr('data-menu-style') == 'accordion') {
+                    $aceMenu.addClass('collapse');
+                    return;
+                }
+                $aceMenu.find('li.menu-active').removeClass('menu-active');
+                $aceMenu.find('ul.slide').removeClass('slide').removeAttr('style');
+            }
+        }
+
+        //ToggleBtn Click
+        $('#menu-btn').click(function () {
+            $aceMenu.slideToggle().toggleClass('hide-menu');
+        });
+
+
+        // Main function 
+        return this.each(function () {
+            // Function for Horizontal menu on mouseenter
+            $aceMenu.on('mouseover', '> li a', function () {
+                if ($aceMenu.hasClass('collapse') === true) {
+                    return false;
+                }
+               // $(this).off('click', '> li a');
+                $(this).parent('li').siblings().children('.sub-menu').stop(true, true).slideUp($animationSpeed).removeClass('slide').removeAttr('style').stop();
+                $(this).parent().addClass('menu-active').children('.sub-menu').slideDown($animationSpeed).addClass('slide');
+                return;
+            });
+            $aceMenu.on('mouseleave', 'li', function () {
+                if ($aceMenu.hasClass('collapse') === true) {
+                    return false;
+                }
+                $(this).off('click', '> li a');
+                $(this).removeClass('menu-active');
+                $(this).children('ul.sub-menu').stop(true, true).slideUp($animationSpeed).removeClass('slide').removeAttr('style');
+                return;
+            });
+            //End of Horizontal menu function
+
+            // Function for Vertical/Responsive Menu on mouse click
+            $aceMenu.on('click', '> li a', function () {
+                if ($aceMenu.hasClass('collapse') === false) {
+                    //return false;
+                }
+                $(this).off('mouseover', '> li a');
+                if ($(this).parent().hasClass('menu-active')) {
+                    $(this).parent().children('.sub-menu').slideUp().removeClass('slide');
+                    $(this).parent().removeClass('menu-active');
+                } else {
+                    if ($expandAll == true) {
+                        $(this).parent().addClass('menu-active').children('.sub-menu').slideDown($animationSpeed).addClass('slide');
+                        return;
+                    }
+                    $(this).parent().siblings().removeClass('menu-active');
+                    $(this).parent('li').siblings().children('.sub-menu').slideUp().removeClass('slide');
+                    $(this).parent().addClass('menu-active').children('.sub-menu').slideDown($animationSpeed).addClass('slide');
+                }
+            });
+            //End of responsive menu function
+
+        });
+        //End of Main function
+    }
+})(jQuery);
+
+$(document).ready(function () {
+    $("#respMenu").aceResponsiveMenu({
+        resizeWidth: '992', // Set the same in Media query
+        animationSpeed: 'fast', //slow, medium, fast
+        accoridonExpAll: false //Expands all the accordion menu on click
+    });
+});
+
+//===== Sticky
+new WOW().init();
+$(window).on('scroll', function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll < 145) {
+        $(".header-bottom").removeClass("sticky");
+        //$(".header-three .navigation img").attr("src", "images/logo-2.png");
+    } else {
+        $(".header-bottom").addClass("sticky");
+        //$(".header-three .navigation img").attr("src", "images/logo.png");
+    }
+});
